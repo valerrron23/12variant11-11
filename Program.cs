490383a8eb6aedc20fb9dd33d175ac7ce13c9771
Михайлова Валерия Романовна <114227035+valerrron23@users.noreply.github.com>
2022-11-12 19:50:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace laba10
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("X: ");
+            double X = double.Parse(Console.ReadLine()); //вводим значение переменной ряда
+            Console.Write("N: ");
+            int N = int.Parse(Console.ReadLine());//вводим верхнее число ряда
+            Console.Write("K: ");
+            int K = int.Parse(Console.ReadLine()); //вводим нижнее число ряда
+            double sum = 0; //задаем целочисленную переменную
+            for (; K - 1 < N; K++) //складываем члены ряда до n
+            {
+                sum += Math.Pow(X, K) * Math.Cos(K * Math.PI / 3); //вычисляем член ряда
+            }
+            Console.WriteLine($"Сумма {N} членов ряда равна : " + sum); //вывод результата
+        }
+    }
+}
